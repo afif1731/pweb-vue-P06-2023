9b7a2b5a0f3554e77827f48d110c5efda6c048aa
Fazrul Ahmad Fadhilah <fazrulfadhila9@gmail.com>
2023-10-02 20:09:43 +0700

    add header

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,59 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+    <header>
+      <h1>{{ title }}</h1>
+      <nav>
+        <ul>
+          <li><a href="#"><router-link :to="{name: 'home'}">Beranda</router-link>></a></li>
+          <li><a href="#"><router-link :to="{name: 'about'}">Tentang</router-link></a></li>
+        </ul>
+      </nav>
+    </header>
+ 
+    <RouterView />
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        title: "Welcome to my to do list"
+      };
+    }
+  }
+  </script>
+  
+  <style scoped>
+header {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+}
 
+h1 {
+  font-size: 36px;
+  background-color: #333;
+}
+
+nav ul {
+  list-style: none;
+  background-color: #333;
+  padding: 0;
+}
+
+nav ul li {
+  display: inline;
+  background-color: #333;
+  margin-right: 20px;
+}
+
+nav ul li a {
+  text-decoration: none;
+  background-color: #333;
+  color: #fff;
+}
+  </style>
 <style>
 * {
   margin: 0;
